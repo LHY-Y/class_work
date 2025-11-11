49bd3d9cf4fc232ea0628754b8864cf3836a28bc
LHY-Y <thunder0307@gw.seoularts.ac.kr>
2025-11-11 11:52:31 +0900

    refactoring(clean)
    edit class Particle, ParticleSystem

--- Particle.js
+++ Particle.js
@@ -1,32 +1,33 @@
-// A simple Particle class
-let Particle = function(position) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
-  this.position = position.copy();
-  this.lifespan = 255;
-};
+class Particle {
 
-Particle.prototype.run = function() {
-  this.update();
-  this.display();
-};
+    constructor(position) {
+    this.acceleration = createVector(0, 0.05);
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.position = position.copy();
+    this.lifespan = 255;}
 
-// Method to update position
-Particle.prototype.update = function(){
-  this.velocity.add(this.acceleration);
-  this.position.add(this.velocity);
-  this.lifespan -= 2;
-};
+    run() {
+    this.update();
+    this.display();
+    }
 
-// Method to display
-Particle.prototype.display = function() {
-  stroke(200, this.lifespan);
-  strokeWeight(2);
-  fill(127, this.lifespan);
-  ellipse(this.position.x, this.position.y, 12, 12);
-};
+    update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+    }
 
-// Is the particle still useful?
-Particle.prototype.isDead = function(){
-  return this.lifespan < 0;
-};
+    display() {
+    stroke(200, this.lifespan);
+    strokeWeight(2);
+    fill(127, this.lifespan);
+    ellipse(this.position.x, this.position.y, 12, 12);
+    }
+
+    isDead() {
+    return this.lifespan < 0;
+    }
+}
+
+
+
--- ParticleSystem.js
+++ ParticleSystem.js
@@ -1,18 +1,23 @@
 //ParticleSystem
-let ParticleSystem = function(position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
 
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
+class ParticleSystem {
+    constructor(position) {
+        this.origin = position.copy();
+        this.particles = [];
+    }
 
-ParticleSystem.prototype.run = function() {
-  for (let i = this.particles.length-1; i >= 0; i--) {
+    addParticle() {
+    this.particles.push(new Particle(this.origin));
+    }
+
+    run() {
+    for (let i = this.particles.length-1; i >= 0; i--) {
     let p = this.particles[i];
     p.run();
     if (p.isDead()) {
       this.particles.splice(i, 1);
     }
-  }}
+  }       
+    }
+
+}
