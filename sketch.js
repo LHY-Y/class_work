1da59d86b1e9af0092a934d68c268c7ed01ad6b7
LHY-Y <thunder0307@gw.seoularts.ac.kr>
2025-11-12 19:51:21 +0900

    add 'wind'
    applyForce를 이용해 wind도 구현할 수 있도록 추가했다.

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let system;
 
 function setup() {
   createCanvas(720, 400);
-  system = new ParticleSystem(createVector(width / 2, 50));
+  system = new ParticleSystem(createVector(width / 2, height/5*2));
 }
 
 function draw() {
@@ -10,6 +10,13 @@ function draw() {
 
   let gravity = createVector(0, 0.05);
   system.applyForce(gravity);
+
+  if (mouseIsPressed) {
+    let wind = p5.Vector.sub(createVector(mouseX, mouseY), system.origin);
+    wind.normalize();
+    wind.mult(0.2);
+    system.applyForce(wind);
+  }
 
   system.addParticle();
   system.run();
